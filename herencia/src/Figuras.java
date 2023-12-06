de0a5d8e5d86bba58651f5f5cd6057dbefa56475
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2023-12-05 23:38:04 -0500

    clase figuras

---
+++ herencia/src/Figuras.java
@@ -0,0 +1,30 @@
+public class Figuras {
+    String nombre;
+    int lados;
+
+    //siempre debe ir el constructor
+    public Figuras(String nombre, int lados) {
+        this.nombre = nombre;
+        this.lados = lados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getLados() {
+        return lados;
+    }
+
+    public void setLados(int lados) {
+        this.lados = lados;
+    }
+    //metodo personalizado
+    public void tipoFigura(){
+        System.out.println(" Esto es un " + this.getNombre());
+    }
+}
