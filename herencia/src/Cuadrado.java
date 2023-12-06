3fae1dd252d72354f9a33dd34e54025ed0612e74
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-12-06 18:22:16 -0500

    Corr4eccion y mejoramiento

--- herencia/src/Cuadrado.java
+++ herencia/src/Cuadrado.java
@@ -2,4 +2,14 @@ public class Cuadrado extends Poligono{
     public Cuadrado(String nombre, int lados) {
         super(nombre, lados);
     }
+    public void area()
+    {
+        double area=Math.pow(this.lados, 2);
+        System.out.printf("El area del cuadrado es: %.2f\n", area);
+    }
+    public void perimetro()
+    {
+        double perimetro=this.lados*4;
+        System.out.printf("El perimetro del cuadrado es: %.2f\n", perimetro);
+    }
 }
--- herencia/src/Cubo.java
+++ herencia/src/Cubo.java
@@ -2,5 +2,11 @@ public class Cubo extends Poligono {
     public Cubo(String nombre, int lados) {
         super(nombre, lados);
     }
-    
+    public void area(){
+        System.out.println("El area de un cubo es: " + (this.lados*this.lados)*6);
+    }
+
+    public void perimetro(){
+        System.out.println("El perimetro de un cubo es: " + this.lados*12);
+    }
 }
