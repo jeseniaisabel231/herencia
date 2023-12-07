cc967937c1450e3b264752d19a0d4c975cc10287
Estefanía Sánchez <estefi2000ms2@gmail.com>
2023-12-06 20:27:30 -0500

    version actualizada 2
    actualización de rectángulo

--- herencia/src/Rectangulo.java
+++ herencia/src/Rectangulo.java
@@ -2,4 +2,10 @@ public class Rectangulo extends Irregulares{
     public Rectangulo(String nombre, int lados) {
         super(nombre, lados);
     }
+    public void perimetroRec(){
+        System.out.println("El perimetro es: " + (this.getAltura()*2)+(2*this.getBase()));
+    }
+    public void areaRec(){
+        System.out.println("El area es: " + this.getBase()*this.getArea());
+    }
 }
